77ed1b93179b8d0ea60c7d9037d12ef6a6e03d98
tohoohome <62142195+tohoohome@users.noreply.github.com>
2024-10-28 00:03:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-const fs =require('fs');
-let data;
-fs.readFile('index.html', 'utf8', function (err, datat) {
-    // Display the file content
-    data=datat
-});
-let now = new Date();
-const test="Current Date and Time:" + now.toString();
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
 
-const http = require('http');
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-const port = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Wesam!\n'
-  res.end(test);
+// Serve the HTML file as the home page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+// Endpoint to serve SSE
+app.get('/events', (req, res) => {
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+
+    // Initial event to confirm connection
+    res.write(`data: Connected\n\n`);
+
+    // Send data every 2 seconds
+    const intervalId = setInterval(() => {
+        const now = new Date().toLocaleTimeString();
+        res.write(`data: ${now}\n\n`);
+    }, 2000);
+
+    // Close the connection when the client disconnects
+    req.on('close', () => {
+        clearInterval(intervalId);
+        res.end();
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
