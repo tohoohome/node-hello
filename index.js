940c70aa9ee7477e0edf01406741e03bdac65f98
tohoohome <62142195+tohoohome@users.noreply.github.com>
2024-10-27 20:40:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const fs =require('fs');
+6const fs =require('fs');
 let data;
 fs.readFile('index.html', 'utf8', function (err, datat) {
     // Display the file content
     data=datat
 });
+const now = new Date();
+const test="Current Date and Time:", now.toString();
 
 const http = require('http');
 
@@ -12,7 +14,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Wesam!\n'
-  res.end(data);
+  res.end(test);
 });
 
 server.listen(port, () => {
