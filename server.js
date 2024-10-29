2765a2ddf8cceeb273ef68a06210287d0f5efb41
tohoohome <62142195+tohoohome@users.noreply.github.com>
2024-10-29 14:13:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/api/items', async(req, res) => {
         const allItems = await itemsPool.query(
             'SELECT * FROM items'
         );
-       res.json({ allItems.rows });
+       res.json({ items: allItems.rows });
        // res.send(`Done ${allItems}`);
     } catch (error) {
         console.log(error);
